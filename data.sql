4301cc7c4cc12593df07e9cd9bcc16ef69becfe1
tuneshell <tuneshell@users.noreply.github.com>
2018-10-24 10:03:06 +0300

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,23 @@
+insert into bank_acc(id, amount) values (1, 10000);
+
+insert into client (id, name, phone) values (1, 'John Doe', '3-555-123456');
+
+insert into client (id, name, phone) values (2, 'Eleanor Rigby', '3-555-000123');
+
+insert into client (id, name, phone) values (3, 'Ed Wood', '1-999-139028');
+
+insert into client_acc(id, amount, owner) values (1, 2000, 1);
+
+insert into client_acc(id, amount, owner) values (2, 500, 1);
+
+insert into client_acc(id, amount, owner) values (3, 150, 1);
+
+insert into client_acc(id, amount, owner) values (4, 1000, 2);
+
+insert into client_acc(id, amount, owner) values (5, 600, 3);
+
+insert into client_acc(id, amount, owner) values (6, 300, 3);
+
+
+
+
